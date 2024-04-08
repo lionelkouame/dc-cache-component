f6c1a5175f41a746e7dd19847256df33c6aced76
lionelkouame <lionnelkouame@gmail.com>
2024-04-08 06:35:40 +0200

    #2 - use cache

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -7,14 +7,19 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class HomepageController extends AbstractController
 {
 
     #[Route('/homepage', name: 'app_homepage')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntityManagerInterface $entityManager, CacheInterface $cache,): Response
     {
-        $posts = $entityManager->getRepository(Post::class)->findAll();
+        $posts = $cache->get('homepage_posts', function (ItemInterface $item) use ($entityManager) {
+            $item->expiresAfter(10);
+            return $entityManager->getRepository(Post::class)->findAll();
+        });
 
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
